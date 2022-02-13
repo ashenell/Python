e905d2795a21ea58899af3607d906f0bb83a8c28
ashenell <79301820+ashenell@users.noreply.github.com>
2022-02-13 19:54:23 +0200

    Update generate_random_csv.py

--- generate_random_csv.py
+++ generate_random_csv.py
@@ -33,47 +33,31 @@ while True:
     except ValueError:
         cols = 10
         break
-while True:
-    try:
-        nums = int(input('Each multiple column is a number column, cant be 0 min 3/max 5 \n Enter the value: '))
-    except ValueError:
-        print('Try again')
-        continue
-    if nums <= 2 or nums >=6:
-        print('Sorry, your response did not mach. Min value can be 3 and max 5.')
-        continue
-    else:
-        break
 dest = str(input('Enter the file name, must write with file type: ') or 'Create-MyCSV.csv') #Asking for a user file name, if we dont enter the value, uses the default
-line = 0
-line1 = 0
 #Function for random strings and integers
 def id_generator(size= char, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-def nr_generator(size= char, chars=string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
+def nr_generator(size= char, chars=string.octdigits):
+    return ''.join(random.choice(chars) for x in range(size))
 print(nr_generator())
 print(id_generator())
-uusa = []
-uusa1 = []
-nums1 = nums -1
-
-for i in range(rows * cols):
-    line += 1
-    uusa1.append(id_generator())
-    
-    if line == cols:
-        uusa.append(uusa1)
-        line = 0
-        uusa1 = []
-        
-uusa[nums1::nums] = [nr_generator() for x in uusa[nums1::nums]]
-print(uusa)
-
-def write_csv_file(filename: dest, data: uusa):
-        with open(filename, "w", newline="") as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=";")
-            for row in data:
-                csv_writer.writerow(row)
-write_csv_file(dest, uusa)
+nums = int(input('Each multiple column is a number column min 3/max 5 \n Enter the value: '))
+if nums == 3 or nums ==4 or nums ==5:
+    data = [[ nr_generator() if r%nums==nums-1 else id_generator()     
+                for _ in range(cols)]
+             for r in range(rows)]
+else:
+    data = []
+    for i in range(rows):
+        sublist = []
+        for y in range(cols):
+            sublist.append(id_generator())   
+        data.append(sublist)
+ 
+print(data)
+def write_lines_to_file(filename: dest, lines: data):
+    with open(filename, "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=";")
+        for row in data:
+            csv_writer.writerow(row)
+write_lines_to_file(dest,data)
